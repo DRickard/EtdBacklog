929c5110e44031f30cc9a7946a5ec802bc0349ae
DRickard <drickard1967@library.ucla.edu>
2015-07-15 14:22:19 -0700

    Create etds_backlog.sql

---
+++ etds_backlog.sql
@@ -0,0 +1,12 @@
+-- other settings via vger_sqlplus_run
+SET linesize 200;
+
+SELECT 
+        record_id || ':' ||
+        proq_id
+FROM 
+        vger_support.backlog_etds 
+WHERE 
+	embargo_code=&1
+        AND processed IS NULL
+/
